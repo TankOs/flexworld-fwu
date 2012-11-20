20b17235c6e963fbd454261e3aef31332c390356
Stefan Schindler <stefan@boxbox.org>
2012-11-20 22:51:39 +0100

    Rewrote axis constants.

--- include/FWU/Axis.hpp
+++ include/FWU/Axis.hpp
@@ -3,40 +3,11 @@
 #include <SFML/System/Vector3.hpp>
 
 namespace util {
+namespace axis { ///< Axis vectors.
 
-/** Axis.
- */
-enum Axis : int {
-	X = 0, ///< X axis.
-	Y = 1, ///< Y axis.
-	Z = 2 ///< Z axis.
-};
-
-/** Construct axis vector.
- * @tparam axis Axis.
- * @return Axis vector.
- */
-template <int axis>
-const sf::Vector3f& axis_vector();
-
-/** Construct x axis vector.
- * @return X axis vector.
- */
-template <>
-const sf::Vector3f& axis_vector<Axis::X>();
-
-/** Construct y axis vector.
- * @return Y axis vector.
- */
-template <>
-const sf::Vector3f& axis_vector<Axis::Y>();
-
-/** Construct z axis vector.
- * @return Z axis vector.
- */
-template <>
-const sf::Vector3f& axis_vector<Axis::Z>();
+static const sf::Vector3f x = sf::Vector3f( 1.0f, 0.0f, 0.0f ); ///< X axis.
+static const sf::Vector3f y = sf::Vector3f( 0.0f, 1.0f, 0.0f ); ///< Y axis.
+static const sf::Vector3f z = sf::Vector3f( 0.0f, 0.0f, 1.0f ); ///< Z axis.
 
 }
-
-#include "Axis.inl"
+}
--- test/src/TestAxis.cpp
+++ test/src/TestAxis.cpp
@@ -7,8 +7,8 @@ using namespace util;
 BOOST_AUTO_TEST_CASE( TestAxis ) {
 	// Initial state.
 	{
-		BOOST_CHECK( axis_vector<Axis::X>() == sf::Vector3f( 1.0f, 0.0f, 0.0f ) );
-		BOOST_CHECK( axis_vector<Axis::Y>() == sf::Vector3f( 0.0f, 1.0f, 0.0f ) );
-		BOOST_CHECK( axis_vector<Axis::Z>() == sf::Vector3f( 0.0f, 0.0f, 1.0f ) );
+		BOOST_CHECK( axis::x == sf::Vector3f( 1.0f, 0.0f, 0.0f ) );
+		BOOST_CHECK( axis::y == sf::Vector3f( 0.0f, 1.0f, 0.0f ) );
+		BOOST_CHECK( axis::z == sf::Vector3f( 0.0f, 0.0f, 1.0f ) );
 	}
 }
